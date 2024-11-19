c1eb85ae407a3491fe474628a6fcba782c5a3650
unknown <k3kc228@gmail.com>
2024-11-19 14:38:45 +0600

    Filters for ToDo added

---
+++ src/components/TodoFilters.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <button @click="setFilter('all')" :class="{ active: filter === 'all' }">
+      Все
+    </button>
+    <button
+      @click="setFilter('active')"
+      :class="{ active: filter === 'active' }"
+    >
+      Активные
+    </button>
+    <button
+      @click="setFilter('completed')"
+      :class="{ active: filter === 'completed' }"
+    >
+      Завершенные
+    </button>
+    <span>Осталось: {{ remaining }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    filter: {
+      type: String,
+      required: true,
+    },
+    remaining: {
+      type: Number,
+      required: true,
+    },
+  },
+  methods: {
+    setFilter(filter) {
+      this.$emit("setFilter", filter);
+    },
+  },
+};
+</script>
+
+<style>
+button.active {
+  font-weight: bold;
+}
+</style>
